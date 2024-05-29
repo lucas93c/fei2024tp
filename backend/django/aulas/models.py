5fe53f4ba495e865888185ebca2ef7806c6267a3
lucas93c <lucascabralpr@gmail.com>
2024-05-28 22:42:35 -0300

    TP Nº2 Ej. 4 Creacion de modelos

--- backend/django/aulas/models.py
+++ backend/django/aulas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Carrera(models.Model):
+    id = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=128, null=False)
+
+class Profesor(models.Model):
+    id = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=128, null=False)
+    apellido = models.CharField(max_length=128, null=False)
+    mostrar = models.CharField(max_length=256, null=False)
+
+class Materia(models.Model):
+    id = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=128, null=False)
+    cant_alumnos = models.IntegerField(default=5, null=False)
+    id_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+class Aula(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=128, null=False)
+    ubicacion = models.CharField(max_length=128, null=False)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
+    
